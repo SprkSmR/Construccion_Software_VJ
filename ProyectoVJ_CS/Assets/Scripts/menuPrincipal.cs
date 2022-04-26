1ae0a1c5b87f23fce473d3ee8a290bd34dd8c20d
SprkSmR <chitox_ji_armen@hotmail.com>
2022-04-26 17:31:52 -0500

    Corrección menú principal

--- ProyectoVJ_CS/Assets/Scripts/menuPrincipal.cs
+++ ProyectoVJ_CS/Assets/Scripts/menuPrincipal.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class menuPrincipal : MonoBehaviour
 {
     public void Comenzar(){
-        SceneManager.LoadScene("Scenes/nivelPruebas");
+        SceneManager.LoadScene("Scenes/nivel1");
         Time.timeScale = 1f;
     }
 
